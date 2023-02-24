68ca9d7c2391389beacb774f851a92549981c8cb
hshwang <hshwang@fxgear.net>
2023-02-24 11:40:13 +0900

    먼저 setUp 호출하기 할일 완료

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -4,10 +4,16 @@ from WasRun import WasRun
 
 class TestCaseTest(TestCase):
 
-    def testRunning(self):
-        test = WasRun('testMethod')
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+    def setUp(self):
+        self.test = WasRun('testMethod')
 
+    def testSetUp(self):
+        self.test.run()
+        assert self.test.wasSetUp
+
+    def testRunning(self):
+        self.test.run()
+        assert self.test.wasRun
+
+TestCaseTest('testSetUp').run()
 TestCaseTest('testRunning').run()
--- WasRun.py
+++ WasRun.py
@@ -7,5 +7,9 @@ class WasRun(TestCase):
         self.wasRun = None
         TestCase.__init__(self, name)
 
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+
     def testMethod(self):
         self.wasRun = 1 
