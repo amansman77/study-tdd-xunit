8b668299a9d2e46aaf1329ab7c0374012dbceace
hshwang <hshwang@fxgear.net>
2023-02-24 12:07:45 +0900

    실패한 테스트 보고하기 할일 완료

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -1,4 +1,5 @@
 from TestCase import TestCase
+from TestResult import TestResult
 from WasRun import WasRun
 
 
@@ -17,6 +18,12 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert '1 run, 0 failed' == result.summary()
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert '1 run, 1 failed' == result.summary()
+
     def testFailedResult(self):
         test = WasRun('testBrokenMethod')
         result = test.run()
@@ -24,4 +31,5 @@ class TestCaseTest(TestCase):
 
 TestCaseTest('testTemplateMethod').run()
 TestCaseTest('testResult').run()
-# TestCaseTest('testFailedResult').run()
+TestCaseTest('testFailedResultFormatting').run()
+TestCaseTest('testFailedResult').run()
